10c498921481ba2fc841c24f4af7f4fdf84c182a
Noor <noormahammad@users.noreply.github.com>
2020-02-23 21:11:55 -0500

    Storing data in the local database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,6 @@
 export const ADD_PLACE = 'ADD_PLACE ';
 import * as FileSystem from 'expo-file-system';
+import { insertPlace } from '../helpers/db';
 
 export const addPlace = (title, imageUri) => {  
   return async dispatch => {
@@ -10,17 +11,20 @@ export const addPlace = (title, imageUri) => {
         from: imageUri,
         to: newImageUri
       })
+      const dbResult = await insertPlace(title, newImageUri, 'Dummy address', 15.3, 18.5);
+      console.log("dbResult.insertId: " + dbResult.insertId)
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE, 
+        placeData: {
+          id: dbResult.insertId,
+          title: title,
+          imageUri: newImageUri
+        }
+      });
     }catch(err) {
       console.log(err);
       throw err;
-    }
-
-    dispatch({
-      type: ADD_PLACE, 
-      placeData: {
-        title: title,
-        imageUri: newImageUri
-      }
-    });
+    }    
   }
 }
