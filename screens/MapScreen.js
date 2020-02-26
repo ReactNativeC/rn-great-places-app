9c3b928d56832a856d0d55c0bc65e9f003e88f74
Noor <noormahammad@users.noreply.github.com>
2020-02-26 06:38:31 -0500

    Adding a marker on interactive map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Dimensions, NativeEventEmitter } from 'react-native';
 import { HeaderButtons, Item} from 'react-navigation-header-buttons';
 import HeaderButton from '../components/UI/HeaderButton';
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 const MapScreen = () => {
+  [selectedLocation, setSelectedLocation] = useState();
   const mapRegion = {
     latitude: 40.744080, 
     longitude: -73.936073, 
     latitudeDelta: 0.0922, 
     longitudeDelta: 0.0421
   }
+  const onLocationPicked = event => {
+    setSelectedLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude
+    })    
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapStyle} region={mapRegion} />
+      <MapView style={styles.mapStyle} region={mapRegion} onPress={onLocationPicked}>
+      { selectedLocation && <Marker title="You picked this location" coordinate={selectedLocation} /> }
+      </MapView>
     </View>
   )
 }
